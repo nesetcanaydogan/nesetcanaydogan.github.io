8ea02aa6c9f577e39977f0d0ad1763e1b24147e1
canaydogan <canaydogan0568@gmail.com>
2025-01-19 16:59:29 +0300

    Style updates

---
+++ app.js
@@ -0,0 +1,22 @@
+// const button = document.querySelector('button')
+// const textList = ["NO!", "STOP!", "GO AWAY!"]
+
+// button.addEventListener('mouseenter', function () {
+// })
+
+// button.addEventListener("mouseenter", () => {
+//     // Sayfa genişliği ve yüksekliği
+//     const pageWidth = window.innerWidth;
+//     const pageHeight = window.innerHeight;
+
+//     // Rastgele koordinatlar (elementin boyutunu hesaba katmak için offset düşülüyor)
+//     const randomX = Math.floor(Math.random() * (pageWidth - button.offsetWidth));
+//     const randomY = Math.floor(Math.random() * (pageHeight - button.offsetHeight));
+
+//     // Yeni konumu ayarlayın
+//     button.style.position = "absolute";
+//     button.style.left = `${randomX}px`;
+//     button.style.top = `${randomY}px`;
+// });
+
+
